b54bd9f68c7449982fbef6a0dad7e41e8d2d136e
Shamus Schneider <67710582+Sh-wayz@users.noreply.github.com>
2020-08-29 21:15:12 -0400

    Update Constants.py

--- toascii/Constants.py
+++ toascii/Constants.py
@@ -3,5 +3,7 @@ gradients = (  # gradients which can be chosen when creating a video object late
     ' `-~+#@',  # good for high light/contrast environments
     ' `\'"*+=g$@',  # low light low contrast environments
     ' ░▒▓█',
-    'ÆÑÊŒØMÉËÈÃÂWQBÅæ#NÁþEÄÀHKRŽœXgÐêqÛŠÕÔA€ßpmãâG¶øðé8ÚÜ$ëdÙýèÓÞÖåÿÒb¥FDñáZPäšÇàhû§ÝkŸ®S9žUTe6µOyxÎ¾f4õ5ôú&aü™2ùçw©Y£0VÍL±3ÏÌóC@nöòs¢u‰½¼‡zJƒ%¤Itocîrjv1lí=ïì<>i7†[¿?×}*{+()\/»«•¬|!¡÷¦¯—^ª„”“~³º²–°­¹‹›;:’‘‚’˜ˆ¸…·¨´`'
+    'ÆÑÊŒØMÉËÈÃÂWQBÅæ#NÁþEÄÀHKRŽœXgÐêqÛŠÕÔA€ßpmãâG¶øðé8ÚÜ$ëdÙýèÓÞÖåÿÒb¥FDñáZPäšÇàhû§ÝkŸ®S9žUTe6µOyxÎ¾f4õ5ôú&aü™2ùçw©Y£0VÍL±3ÏÌóC@nöòs¢u‰½¼‡zJƒ%¤Itocîrjv1lí=ïì<>i7†[¿?×}*{+()\/»«•¬|!¡÷¦¯—^ª„”“~³º²–°­¹‹›;:’‘‚’˜ˆ¸…·¨´`',
+    ' ¨\'³•†µðEÆ'
+    
 )
